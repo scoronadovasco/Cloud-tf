2ccabc21cc17260fd5a033e708a133c4634bfa74
scoronadovasco <93351224+scoronadovasco@users.noreply.github.com>
2025-04-04 10:50:01 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -69,7 +69,6 @@ resource "aws_security_group" "sg_public_instance" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    ipv6_cidr_blocks = ["::/0"]
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
